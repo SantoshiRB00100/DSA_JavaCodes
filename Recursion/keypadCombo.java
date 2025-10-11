7f3f039d3d33ceb38959cb5b08a5c415423f4552
SantoshiRB00100 <srbalingol@gmail.com>
2025-10-11 19:30:20 +0530

    keypad combination

---
+++ Recursion/keypadCombo.java
@@ -0,0 +1,22 @@
+package DSA_JavaCodes.Recursion;
+
+public class keypadCombo {
+    public static String[] keypad = {".","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public static void printComb(String str, int idx, String combo){
+        if(idx == str.length()){
+            System.out.println(combo);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        String mapping = keypad[currChar - '0'];
+
+        for(int i=0;i<mapping.length();i++){
+           printComb(str, idx+1, combo + mapping.charAt(i));
+        }
+    }    
+    public static void main(String[] args) {
+        String str = "23";
+        printComb(str,0,"");
+    }
+}
